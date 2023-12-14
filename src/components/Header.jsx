2a6b355a2e95adbdd437325bcbd38132aa299cf7
arkamaldeen <kabdurrahmaan28@gmail.com>
2023-12-14 15:55:40 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import HighlightIcon from '@mui/icons-material/Highlight';
+
+function Header() {
+    return (
+        <header>
+            <h1>
+                <HighlightIcon />
+                Keeper App
+            </h1>
+        </header>
+    );
+}
+
+export default Header;
